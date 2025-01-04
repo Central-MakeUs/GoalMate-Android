2bf4c6b3063919b41b102f580ca940bb9a34ca84
JoYehyun <jo990714@naver.com>
2025-01-04 22:10:40 +0900

    chore: 폰트 설정 세팅

---
+++ app/src/main/java/cmc/goalmate/ui/theme/GoalMateTypography.kt
@@ -0,0 +1,125 @@
+package cmc.goalmate.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import cmc.goalmate.R
+
+private val pretendardBold =
+    FontFamily(
+        Font(R.font.pretendard_bold, FontWeight.Bold, FontStyle.Normal),
+    )
+
+private val pretendardRegular =
+    FontFamily(
+        Font(R.font.pretendard_regular, FontWeight.Normal, FontStyle.Normal),
+    )
+
+data class GoalMateTypography(
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val h3: TextStyle,
+    val h4: TextStyle,
+    val subtitle: TextStyle,
+    val subtitleSmall: TextStyle,
+    val body: TextStyle,
+    val bodySmall: TextStyle,
+    val buttonLabelLarge: TextStyle,
+    val buttonLabelSmall: TextStyle,
+    val caption: TextStyle,
+)
+
+private val H1 =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 36.sp,
+        lineHeight = 46.8.sp,
+    )
+
+private val H2 =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 32.sp,
+        lineHeight = 41.6.sp,
+    )
+
+private val H3 =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 28.sp,
+        lineHeight = 36.4.sp,
+    )
+
+private val H4 =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 24.sp,
+        lineHeight = 31.2.sp,
+    )
+
+private val Subtitle =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 20.sp,
+        lineHeight = 26.sp,
+    )
+
+private val SubtitleSmall =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 18.sp,
+        lineHeight = 23.4.sp,
+    )
+
+private val Body =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+    )
+
+private val BodySmall =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 14.sp,
+        lineHeight = 21.sp,
+    )
+
+private val ButtonLabelLarge =
+    TextStyle(
+        fontFamily = pretendardBold,
+        fontSize = 16.sp,
+        lineHeight = 25.6.sp,
+    )
+
+private val ButtonLabelSmall =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 14.sp,
+        lineHeight = 14.sp,
+    )
+
+private val Caption =
+    TextStyle(
+        fontFamily = pretendardRegular,
+        fontSize = 12.sp,
+        lineHeight = 12.sp,
+    )
+
+val goalMateTypography =
+    GoalMateTypography(
+        h1 = H1,
+        h2 = H2,
+        h3 = H3,
+        h4 = H4,
+        subtitle = Subtitle,
+        subtitleSmall = SubtitleSmall,
+        body = Body,
+        bodySmall = BodySmall,
+        buttonLabelLarge = ButtonLabelLarge,
+        buttonLabelSmall = ButtonLabelSmall,
+        caption = Caption,
+    )
--- app/src/main/java/cmc/goalmate/ui/theme/Theme.kt
+++ app/src/main/java/cmc/goalmate/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package cmc.goalmate.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,19 +8,23 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+private val DarkColorScheme =
+    darkColorScheme(
+        primary = Purple80,
+        secondary = PurpleGrey80,
+        tertiary = Pink80,
+    )
 
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
+private val LightColorScheme =
+    lightColorScheme(
+        primary = Purple40,
+        secondary = PurpleGrey40,
+        tertiary = Pink40,
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
     surface = Color(0xFFFFFBFE),
@@ -30,29 +33,37 @@ private val LightColorScheme = lightColorScheme(
     onTertiary = Color.White,
     onBackground = Color(0xFF1C1B1F),
     onSurface = Color(0xFF1C1B1F),
-    */
-)
+     */
+    )
+
+private val LocalTypography = staticCompositionLocalOf { goalMateTypography }
 
 @Composable
 fun GoalMateTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    val colorScheme =
+        when {
+            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                val context = LocalContext.current
+                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            }
+
+            darkTheme -> DarkColorScheme
+            else -> LightColorScheme
         }
 
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider(LocalTypography provides goalMateTypography) {
+        MaterialTheme(
+            colorScheme = colorScheme,
+            content = content,
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
+
+val MaterialTheme.goalMateTypography: GoalMateTypography
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalTypography.current
