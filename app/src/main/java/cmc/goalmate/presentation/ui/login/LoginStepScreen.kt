cd77e4a72705a0f6b23e7f4328712c9f85345c99
JoYehyun <jo990714@naver.com>
2025-01-14 00:24:03 +0900

    feat: 닉네임 설정 화면 진입 시 키보드 자동으로 올라오도록 설정

--- app/src/main/java/cmc/goalmate/presentation/ui/login/LoginStepScreen.kt
+++ app/src/main/java/cmc/goalmate/presentation/ui/login/LoginStepScreen.kt
@@ -14,10 +14,15 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
@@ -82,6 +87,14 @@ fun NickNameSettingScreen(
     onCompletedButtonClicked: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+        keyboardController?.show()
+    }
+
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
@@ -103,7 +116,7 @@ fun NickNameSettingScreen(
             onValueChange = onTextChanged,
             canCheckDuplicate = isDuplicationCheckEnabled,
             onDuplicateCheck = onDuplicationCheckButtonClicked,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().focusRequester(focusRequester),
             inputTextState = textValidationState,
             helperText = helperText,
         )
