522a0ec5635d302f08e2a4d6fc598bf8ca0c78f5
JoYehyun <jo990714@naver.com>
2025-02-15 14:33:55 +0900

    feat: 토큰 저장 datastore 싱글톤으로 정의

---
+++ app/src/main/java/cmc/goalmate/local/di/DataStoreModule.kt
@@ -0,0 +1,20 @@
+package cmc.goalmate.local.di
+
+import android.content.Context
+import cmc.goalmate.local.TokenDataStore
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+    @Provides
+    @Singleton
+    fun provideTokenDataStore(
+        @ApplicationContext context: Context,
+    ): TokenDataStore = TokenDataStore(context)
+}
