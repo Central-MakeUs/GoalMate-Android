570ba4ce3d3c449ca16150592afdb94d3d788b2a
JoYehyun <jo990714@naver.com>
2025-02-18 03:24:09 +0900

    feat: 목표 목록 도메인 모델 정의

---
+++ app/src/main/java/cmc/goalmate/domain/model/Goal.kt
@@ -0,0 +1,26 @@
+package cmc.goalmate.domain.model
+
+data class Goals(val goals: List<Goal>)
+
+data class Goal(
+    val id: Int,
+    val title: String,
+    val topic: String,
+    val description: String,
+    val period: Int,
+    val dailyDuration: Int,
+    val participantsLimit: Int,
+    val currentParticipants: Int,
+    val isClosingSoon: Boolean,
+    val goalStatus: GoalStatus,
+    val mentorName: String,
+    val createdAt: String,
+    val updatedAt: String,
+    val mainImage: String,
+)
+
+enum class GoalStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    CLOSED,
+}
