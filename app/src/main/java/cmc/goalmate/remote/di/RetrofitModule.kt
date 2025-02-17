df212215d122b5aa856ff6c356ab73b3fc5fb506
JoYehyun <jo990714@naver.com>
2025-02-17 16:25:58 +0900

    feat: Retrofit 모듈 설정

---
+++ app/src/main/java/cmc/goalmate/remote/di/RetrofitModule.kt
@@ -0,0 +1,67 @@
+package cmc.goalmate.remote.di
+import cmc.goalmate.BuildConfig
+import cmc.goalmate.remote.adapter.GoalMateCallAdapterFactory
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RetrofitModule {
+    @Provides
+    fun provideBaseUrl(): String = BuildConfig.BASE_URL
+
+    @Provides
+    @Singleton
+    fun provideJson(): Json =
+        Json {
+            ignoreUnknownKeys = true
+            coerceInputValues = true
+            encodeDefaults = true
+        }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(json: Json): Converter.Factory = json.asConverterFactory("application/json".toMediaType())
+
+    @Provides
+    @Singleton
+    fun provideGoalMateCallAdapterFactory(): GoalMateCallAdapterFactory = GoalMateCallAdapterFactory()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(
+        baseUrl: String,
+        okHttpClient: OkHttpClient,
+        callAdapterFactory: GoalMateCallAdapterFactory,
+        converterFactory: Converter.Factory,
+    ): Retrofit =
+        Retrofit
+            .Builder()
+            .client(okHttpClient)
+            .baseUrl(baseUrl)
+            .addCallAdapterFactory(callAdapterFactory)
+            .addConverterFactory(converterFactory)
+            .build()
+}
+
+val loggingInterceptor = HttpLoggingInterceptor().apply {
+    level = HttpLoggingInterceptor.Level.BODY
+}
