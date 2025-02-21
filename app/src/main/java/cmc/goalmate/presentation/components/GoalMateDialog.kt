f5fb7f4f1fe3d51f501b10d13911758462cc6062
JoYehyun <jo990714@naver.com>
2025-02-22 01:49:41 +0900

    ui: 아이콘 에러 모달 창 ui 추가

--- app/src/main/java/cmc/goalmate/presentation/components/GoalMateDialog.kt
+++ app/src/main/java/cmc/goalmate/presentation/components/GoalMateDialog.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
@@ -26,9 +27,9 @@ import cmc.goalmate.presentation.theme.goalMateTypography
 
 @Composable
 fun GoalMateDialog(
-    contentText: String,
     buttonText: String,
     onConfirmation: () -> Unit,
+    content: @Composable (Modifier) -> Unit,
 ) {
     Dialog(
         onDismissRequest = {
@@ -46,13 +47,7 @@ fun GoalMateDialog(
                     .padding(vertical = GoalMateDimens.BottomMargin),
             ) {
                 Spacer(Modifier.weight(1f))
-                Text(
-                    text = contentText,
-                    style = MaterialTheme.goalMateTypography.buttonLabelLarge,
-                    color = MaterialTheme.goalMateColors.onSurfaceVariant,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxWidth(),
-                )
+                content(Modifier.fillMaxWidth())
                 Spacer(Modifier.weight(1f))
                 GoalMateButton(
                     content = buttonText,
@@ -69,13 +64,62 @@ fun GoalMateDialog(
 }
 
 @Composable
+fun GoalMateIconDialog(
+    subTitle: String,
+    contentText: String,
+    buttonText: String,
+    onConfirmation: () -> Unit,
+) {
+    GoalMateDialog(
+        buttonText = buttonText,
+        onConfirmation = { onConfirmation() },
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            GoalMateImage(
+                image = R.drawable.icon_warn,
+                modifier = it.size(24.dp)
+            )
+            Spacer(Modifier.size(20.dp))
+            Text(
+                text = subTitle,
+                style = MaterialTheme.goalMateTypography.subtitleMedium,
+                color = MaterialTheme.goalMateColors.onSurface,
+            )
+            Spacer(Modifier.size(10.dp))
+            Text(
+                text = contentText,
+                style = MaterialTheme.goalMateTypography.tagSmall,
+                color = MaterialTheme.goalMateColors.onBackground,
+                textAlign = TextAlign.Center
+            )
+
+        }
+    }
+}
+
+@Composable
 @Preview
 private fun GoalMateDialogPreview() {
     GoalMateTheme {
-        GoalMateDialog(
-            contentText = stringResource(R.string.goal_in_progress_uneditable_warning_message),
-            buttonText = "오늘 목표 완료 하기",
+        GoalMateIconDialog(
+            subTitle = "하루 한 번!",
+            contentText = "하루 1회만 코멘트를 입력할 수 있어요.\n" + "오늘 보낸 코멘트를 수정해주세요",
+            buttonText = "확인했어요",
             onConfirmation = {},
-        )
+            )
+//        GoalMateDialog(
+//            buttonText = "오늘 목표 완료 하기",
+//            onConfirmation = {},
+//        ) {
+//            Text(
+//                text = stringResource(R.string.goal_in_progress_uneditable_warning_message),
+//                style = MaterialTheme.goalMateTypography.buttonLabelLarge,
+//                color = MaterialTheme.goalMateColors.onSurfaceVariant,
+//                textAlign = TextAlign.Center,
+//                modifier = it,
+//            )
+//        }
     }
 }
--- app/src/main/java/cmc/goalmate/presentation/ui/progress/inprogress/InProgressScreen.kt
+++ app/src/main/java/cmc/goalmate/presentation/ui/progress/inprogress/InProgressScreen.kt
@@ -2,6 +2,8 @@ package cmc.goalmate.presentation.ui.progress.inprogress
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -9,6 +11,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import cmc.goalmate.R
@@ -16,6 +19,8 @@ import cmc.goalmate.app.navigation.NavigateToCommentDetail
 import cmc.goalmate.app.navigation.NavigateToGoal
 import cmc.goalmate.presentation.components.AppBarWithBackButton
 import cmc.goalmate.presentation.components.GoalMateDialog
+import cmc.goalmate.presentation.theme.goalMateColors
+import cmc.goalmate.presentation.theme.goalMateTypography
 import cmc.goalmate.presentation.ui.util.ObserveAsEvent
 
 @Composable
@@ -61,11 +66,18 @@ fun InProgressScreen(
 
     if (isDialogVisible) {
         GoalMateDialog(
-            contentText = stringResource(R.string.goal_in_progress_uneditable_warning_message),
             buttonText = "오늘 목표 완료 하기",
             onConfirmation = {
                 isDialogVisible = false
             },
-        )
+        ) {
+            Text(
+                text = stringResource(R.string.goal_in_progress_uneditable_warning_message),
+                style = MaterialTheme.goalMateTypography.buttonLabelLarge,
+                color = MaterialTheme.goalMateColors.onSurfaceVariant,
+                textAlign = TextAlign.Center,
+                modifier = it,
+            )
+        }
     }
 }
